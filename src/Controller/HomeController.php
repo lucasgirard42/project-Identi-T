596e032b8da12b81312e60787c50ceb58c1776d8
Girard lucas <pro.lucas.girard@gmail.com>
2020-11-12 15:55:33 +0100

    for in staff

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Staff;
+use App\Repository\StaffRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,10 +13,15 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(StaffRepository $staffRepository) : Response
     {
+        $staffs = $staffRepository->findAll();
+
         return $this->render('Identi-T.html.twig', [
             'controller_name' => 'HomeController',
+            'staffs' => $staffs
         ]);
     }
+
+    
 }
