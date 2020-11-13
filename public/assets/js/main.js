5d6bb1072101ba91e045a6fcf0e2b65c375e862b
Girard lucas <pro.lucas.girard@gmail.com>
2020-11-13 11:00:14 +0100

    add our prising

---
+++ public/assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($){
+    "use strict";
+    // TOP Menu Sticky
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 400) {
+        $("#sticky-header").removeClass("sticky");
+        $('#back-top').fadeIn(500);
+        } else {
+        $("#sticky-header").addClass("sticky");
+        $('#back-top').fadeIn(500);
+        }
+    });
+})
